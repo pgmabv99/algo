6c9809cf4755d21f16f0df0d4e7307ac11a7f3b3
Alexey Vorovich <alexey.vorovich@gmail.com>
2024-12-31 23:34:40 +0000

    absolute spdk  location to allow ssh as root

--- hello_world/run.sh
+++ hello_world/run.sh
@@ -10,7 +10,7 @@ lsblk
 
 echo "0000:00:04.0" | sudo tee /sys/bus/pci/devices/0000:00:04.0/driver/unbind
 sudo modprobe uio_pci_generic
-sudo env "PCI_ALLOWED=0000:00:04.0" $HOME/spdk/scripts/setup.sh 
-sudo env "PCI_ALLOWED=0000:00:04.0" $HOME/spdk/scripts/setup.sh status
+sudo env "PCI_ALLOWED=0000:00:04.0" /home/ec2-user/spdk/scripts/setup.sh 
+sudo env "PCI_ALLOWED=0000:00:04.0" /home/ec2-user/spdk/scripts/setup.sh status
 export LD_LIBRARY_PATH=../spdk/dpdk/build/lib
 sudo env "DRIVER_OVERRIDE=uio_pci_generic" "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" "RTE_EAL_PCI_WHITELIST=0000:00:04.0" ./hello_world
